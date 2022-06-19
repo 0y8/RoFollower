97a3f5868805360dd4bdeadad45da00d4b35aee7
iSpied <105989306+iSpied@users.noreply.github.com>
2022-06-19 00:11:28 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import colorama
+from colorama import Fore
+import random
+
+print(Fore.BLUE + '██╗     ███████╗')
+print(Fore.BLUE + '██║     ██╔════╝')
+print(Fore.BLUE + '██║     ███████╗')
+print(Fore.BLUE + '██║     ╚════██║')
+print(Fore.BLUE + '███████╗███████║')
+print(Fore.BLUE + '╚══════╝╚══════╝')
+print('Made by Jaymes#3351')
+
+username = input('Username: ')
+
+while True:
+    print(Fore.GREEN + "Followed",username,"With Bot #",random.randint(1000, 25000))
