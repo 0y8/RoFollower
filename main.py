ab2a7f66249942027046c68cbe73bb64e2900b7e
iSpied <105989306+iSpied@users.noreply.github.com>
2022-06-22 18:15:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ print('Made by Jaymes#3351')
 username = input('Username: ')
 
 while True:
-    print(Fore.GREEN + "Followed",username,"With Bot #",random.randint(1, 220000))
+    print(Fore.GREEN + "Followed",username,"With Bot #",random.randint(1, 1220000))
