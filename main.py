8e651ac421d58c51f9964d71e7e5b1c41c84827f
iSpied <105989306+iSpied@users.noreply.github.com>
2022-06-25 16:40:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,42 @@ import colorama
 from colorama import Fore
 import random
 import time
-import sys
+import secrets
+import string
 
-print(Fore.BLUE + '██╗     ███████╗')
-print(Fore.BLUE + '██║     ██╔════╝')
-print(Fore.BLUE + '██║     ███████╗')
-print(Fore.BLUE + '██║     ╚════██║')
-print(Fore.BLUE + '███████╗███████║')
-print(Fore.BLUE + '╚══════╝╚══════╝')
-print('Made by Jaymes#3351')
+# Printing the logo and account stock.
 
-username = input('Username: ')
+print(Fore.BLUE + '██████╗ ███████╗██████╗')
+time.sleep(0.1)
+print(Fore.BLUE + '██╔══██╗██╔════╝██╔══██╗')
+time.sleep(0.1)
+print(Fore.BLUE + '██████╔╝█████╗  ██████╔╝')
+time.sleep(0.1)
+print(Fore.BLUE + '██╔══██╗██╔══╝  ██╔══██╗')
+time.sleep(0.1)
+print(Fore.BLUE + '██║  ██║██║     ██████╔╝')
+time.sleep(0.1)
+print(Fore.BLUE + '╚═╝  ╚═╝╚═╝     ╚═════╝')
+time.sleep(1)
+print('Made by complex#3351')
+time.sleep(1)
+# Generating a random number for the account stock.
+print(Fore.RED + 'Bots in stock:',random.randint(100000, 500000))
+# Waiting one second to ask for the user input.
+time.sleep(1)
+# Getting the user input vars.
+username = input(Fore.BLUE + 'Username: ')
 amount = input('Amount of followers: ')
+# Converting the 'amount' var to an int.
 amount = int(amount)
-
-
+# The string length for the username.
+str_length = 15
+# getting the "amount" of followers the user wants.
 for line in range(amount):
+    # Cooling down the print function.
     time.sleep(0.2)
-    print(Fore.GREEN + "Followed",username,"With Bot #",random.randint(1, 1220000))
+    # Generating a random string for the username.
+    r_string = ''.join(secrets.choice(string.ascii_letters + string.digits)
+                   for i in range(str_length))
+    # Printing out all the vars.
+    print(Fore.GREEN + "Followed",username,"With Bot:",r_string)
