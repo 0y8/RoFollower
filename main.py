7062e794c92b99e07164a6a24d9db82b066a3de6
iSpied <105989306+iSpied@users.noreply.github.com>
2022-06-25 15:42:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import colorama
 from colorama import Fore
 import random
+import time
+import sys
 
 print(Fore.BLUE + '██╗     ███████╗')
 print(Fore.BLUE + '██║     ██╔════╝')
@@ -8,11 +10,13 @@ print(Fore.BLUE + '██║     ███████╗')
 print(Fore.BLUE + '██║     ╚════██║')
 print(Fore.BLUE + '███████╗███████║')
 print(Fore.BLUE + '╚══════╝╚══════╝')
-print('Made by complex#3351')
+print('Made by Jaymes#3351')
 
 username = input('Username: ')
 amount = input('Amount of followers: ')
 amount = int(amount)
 
+
 for line in range(amount):
+    time.sleep(0.2)
     print(Fore.GREEN + "Followed",username,"With Bot #",random.randint(1, 1220000))
